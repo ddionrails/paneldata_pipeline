be8ca8d7ff0b93cb7eed6f3bb411ce43795a6c51
hansendx <hansendx@hu-berlin.de>
2019-08-29 10:27:27 +0200

    Fill setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="paneldata_pipeline",
-    version="0.0.1",
-    url="https://github.com/ddionrails/paneldata_pipeline.git",
+    author="Dominique Hansen",
+    author_email="dhansen@diw.de",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+    ],
     description="Process data for the import into a ddionrails instance.",
-    long_description=open("./README.md").read(),
-    packages=find_packages(),
     install_requires=[],
+    keywords=["preprocessing", "ddionrails", "paneldata", "csv", "humanities"],
+    long_description=open("./README.md").read(),
+    name="paneldata_pipeline",
+    packages=find_packages(),
+    python_requires=">=3.6.0",
+    url="https://github.com/ddionrails/paneldata_pipeline.git",
+    version="0.0.1",
 )
