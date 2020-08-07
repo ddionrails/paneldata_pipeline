1bff84f497f9da2d9133a1eee1512556af3feb52
hansendx <DominiqueHansen@gmx.net>
2020-08-07 14:52:19 +0200

    Refactor topics.py to use new field names

--- paneldata_pipeline/topics.py
+++ paneldata_pipeline/topics.py
@@ -1,4 +1,9 @@
-from ddi.onrails.repos import copy
+import json
+from shutil import copy
+
+import pandas as pd
+
+LANGUAGES = dict(en="", de="_de")
 
 
 class Topic:
@@ -23,24 +28,21 @@ class Topic:
     @classmethod
     def get_by_name(cls, name):
         """Get topic from all_objects by name"""
-        try:
-            return [x for x in cls.all_objects if x.name == name][0]
-        except:
-            return None
+        for topic in cls.all_objects:
+            if topic.name == name:
+                return topic
+        return None
 
     @classmethod
     def get_root_topics(cls):
         """Return topics with no parents (== root topics)"""
-        return [x for x in cls.all_objects if x.parent_name == None]
+        return [x for x in cls.all_objects if x.parent_name is None]
 
     @classmethod
     def add_topics_to_parents(cls):
         for topic in cls.all_objects:
-            try:
-                parent = Topic.get_by_name(topic.parent_name)
-                parent.children.append(topic)
-            except:
-                pass
+            parent = Topic.get_by_name(topic.parent_name)
+            parent.children.append(topic)
 
 
 class Concept:
@@ -77,8 +79,10 @@ class TopicParser:
         self,
         topics_input_csv="metadata/topics.csv",
         concepts_input_csv="metadata/concepts.csv",
-        languages=["en", "de"],
+        languages=None,
     ):
+        if not languages:
+            languages = ["en", "de"]
         self.topics_input_csv = topics_input_csv
         self.concepts_input_csv = concepts_input_csv
         self.topics_data = pd.read_csv(topics_input_csv)
@@ -86,12 +90,12 @@ class TopicParser:
         self.languages = languages
 
     def to_csv(self, topics_output_csv="ddionrails/topics.csv"):
-        os.system("cp %s %s" % (self.topics_input_csv, topics_output_csv))
+        copy(self.topics_input_csv, topics_output_csv)
 
     def to_json(self, topics_output_json="ddionrails/topics.json"):
         json_dict = self._create_json()
-        with open(topics_output_json, "w") as f:
-            f.write(json.dumps(json_dict))
+        with open(topics_output_json, "w") as json_file:
+            json_file.write(json.dumps(json_dict))
 
     def _create_json(self):
         result = []
@@ -101,20 +105,20 @@ class TopicParser:
 
     def _convert_to_dict(self, language):
         for row in self.topics_data.to_dict("records"):
-            if str(row.get("parent_name")) == "nan":
+            if str(row.get("parent")) == "nan":
                 parent_name = None
             else:
-                parent_name = row.get("parent_name")
+                parent_name = row.get("parent")
             Topic(
                 name=row.get("name"),
                 label=row.get("label" + LANGUAGES[language], row.get("name")),
                 parent_name=parent_name,
             )
         for row in self.concepts_data.to_dict("records"):
-            if str(row.get("topic_name", "nan")) != "nan":
+            if str(row.get("name", "nan")) != "nan":
                 Concept(
                     name=row.get("name"),
-                    topic_name=row.get("topic_name"),
+                    topic_name=row.get("topic"),
                     label=row.get("label" + LANGUAGES[language], row.get("name")),
                 )
         Topic.add_topics_to_parents()
@@ -128,14 +132,7 @@ class TopicParser:
         return result
 
 
-def main():
-    print("[INFO] Test topics converter.")
-    copy.f("topics.csv")
-    TopicParser(
-        topics_input_csv="ddionrails/topics.csv",
-        concepts_input_csv="ddionrails/concepts.csv",
-    ).to_json()
-
-
 if __name__ == "__main__":
-    main()
+    tp = TopicParser()
+    tp.to_csv()
+    tp.to_json()
