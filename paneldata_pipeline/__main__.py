8f0d3314d5f0dfb71c28c7f5a47b419925757d45
hansendx <DominiqueHansen@gmx.net>
2020-08-11 15:14:18 +0200

    Add basic argument parser

--- paneldata_pipeline/__main__.py
+++ paneldata_pipeline/__main__.py
@@ -1,6 +1,22 @@
 """ Entrypoint related functions of the package. """
 
+import argparse
+from pathlib import Path
+
 
 def main():
     """ Entrypoint of the package. """
     pass  # pylint: disable=unnecessary-pass
+
+
+def parse_arguments():
+    """Setup arguments and parse them."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-folder", dest="input_folder", help="Path to the ")
+    parser.add_argument("-o", "--output-folder", dest="output_folder")
+    _parsed_arguments = parser.parse_args()
+
+    return {
+        "input_folder": Path(_parsed_arguments.input_folder).resolve(),
+        "output_folder": Path(_parsed_arguments.output_folder).resolve(),
+    }
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,26 @@
 """ Test functionality related to the packages entrypoint. """
+import sys
 import unittest
+from pathlib import Path
+from unittest.mock import patch
 
-from paneldata_pipeline.__main__ import main
+from paneldata_pipeline.__main__ import main, parse_arguments
 
 
 class TestMainModule(unittest.TestCase):  # pylint: disable=missing-docstring
     def test_main_method(self):
         """ Test entrypoint of the package. """
         self.assertIsInstance(main, object)
+
+    def test_parse_arguments(self):
+        """Define the expected arguments of the shell entrypoint."""
+
+        arguments = ["__main__.py", "-i", "./input/folder", "-o", "./output/folder"]
+
+        with patch.object(sys, "argv", arguments):
+            _parsed_arguments = parse_arguments()
+
+        self.assertIn("input_folder", _parsed_arguments.keys())
+        self.assertIn("output_folder", _parsed_arguments.keys())
+        self.assertEqual(Path(arguments[2]).resolve(), _parsed_arguments["input_folder"])
+        self.assertEqual(Path(arguments[4]).resolve(), _parsed_arguments["output_folder"])
