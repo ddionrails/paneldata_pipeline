3b26dbcefb4b1cfa4c2560daa2fd82c0ba5bb834
hansendx <DominiqueHansen@gmx.net>
2020-08-12 10:27:16 +0200

    Set up enforcement of type hints

--- paneldata_pipeline/__main__.py
+++ paneldata_pipeline/__main__.py
@@ -2,17 +2,28 @@
 
 import argparse
 from pathlib import Path
-from typing import Dict
+from typing import TypedDict
 
 from paneldata_pipeline.fill_ddionrails import main as fill_ddionrails
 
 
-def main():
+def main() -> None:
     """ Entrypoint of the package. """
     fill_ddionrails(**parse_arguments())
 
 
-def parse_arguments() -> Dict[str, Path]:
+class CLIArgs(TypedDict, total=False):
+    """Define cli argument types."""
+
+    input_folder: Path
+    output_folder: Path
+    variable_relations: bool
+    unify_instrument_data: bool
+    question_relations: bool
+    topic_tree: bool
+
+
+def parse_arguments() -> CLIArgs:
     """Setup arguments and parse them."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input-folder", help="Path to the ", type=_full_path)
@@ -22,16 +33,17 @@ def parse_arguments() -> Dict[str, Path]:
         "-u", "--unify-instrument-data", action="store_true", default=False
     )
     parser.add_argument("-q", "--question-relations", action="store_true", default=False)
-    parser.add_argument(
-        "-g",
-        "--generate-topic-tree",
-        dest="topic_tree",
-        action="store_true",
-        default=False,
-    )
+    parser.add_argument("-g", "--generate-topic-tree", action="store_true", default=False)
     _parsed_arguments = parser.parse_args()
 
-    return vars(_parsed_arguments)
+    return {
+        "input_folder": _parsed_arguments.input_folder,
+        "output_folder": _parsed_arguments.output_folder,
+        "variable_relations": _parsed_arguments.variable_relations,
+        "unify_instrument_data": _parsed_arguments.unify_instrument_data,
+        "question_relations": _parsed_arguments.question_relations,
+        "topic_tree": _parsed_arguments.generate_topic_tree,
+    }
 
 
 def _full_path(path: str) -> Path:
