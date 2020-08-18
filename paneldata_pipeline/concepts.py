1a04bb8d00e0b77d8b929e37d6c730509bd43a61
hansendx <DominiqueHansen@gmx.net>
2020-08-18 11:29:42 +0200

    Handle questions data in extract_implicit_concepts
    Question data could also contain implicit concepts.
    These are now handled alongside the variable concepts.

--- paneldata_pipeline/concepts.py
+++ paneldata_pipeline/concepts.py
@@ -1,35 +1,52 @@
 """ Data manipulation functionality related to the concepts.csv."""
 import csv
 from pathlib import Path
+from typing import Set
 
 
-def extract_implicit_concepts(concepts_path: Path, variables_path: Path):
-    """Add missing concepts, only present in the variable.csv, to the concepts.csv"""
-    with open(variables_path, "r") as variable_csv:
-        variable_concepts = {
-            row.get("concept", row.get("concept_name"))
-            for row in csv.DictReader(variable_csv)
-        }
+def extract_implicit_concepts(input_path: Path, concepts_path: Path = Path()) -> None:
+    """Add missing concepts, only in variables and questions data, to concepts.csv"""
+    if concepts_path == Path():
+        concepts_path = input_path.joinpath("concepts.csv")
+
+    implicit_concepts: Set[str] = set()
+
+    for path in [
+        input_path.joinpath("variables.csv"),
+        input_path.joinpath("questions.csv"),
+    ]:
+        with open(path, "r") as csv_file:
+            for row in csv.DictReader(csv_file):
+                _concept = row.get("concept", row.get("concept_name"))
+                if _concept is None:
+                    raise EnvironmentError(
+                        f"{concepts_path} is missing the 'concept' field."
+                    )
+                implicit_concepts.add(_concept)
 
     if not concepts_path.exists():
-        default_fields = ["name", "topic", "label_de", "label"]
         with open(concepts_path, "w+") as concepts_csv:
-            dict_writer = csv.DictWriter(concepts_csv, fieldnames=default_fields)
-            dict_writer.writeheader()
+            csv.DictWriter(
+                concepts_csv, fieldnames=["name", "topic", "label_de", "label"]
+            ).writeheader()
 
     with open(concepts_path, "r") as concepts_csv:
-        _reader = csv.DictReader(concepts_csv)
-        concept_csv_content = list(_reader)
-        concept_fields = {field: "" for field in _reader.fieldnames}
-        concepts = {row["name"] for row in concept_csv_content}
-    orphaned_concepts = variable_concepts.difference(concepts)
-    if "" in orphaned_concepts:
-        orphaned_concepts.remove("")
+        concepts_reader = csv.DictReader(concepts_csv)
+        concept_csv_content = list(concepts_reader)
+        if concepts_reader.fieldnames:
+            concept_fields = {name: "" for name in concepts_reader.fieldnames}
+        else:
+            raise EnvironmentError(f"{concepts_path} is not a correct concept CSV file.")
+        explicit_concepts = {row["name"] for row in concept_csv_content}
+
+    implicit_concepts.difference_update(explicit_concepts)
+    if "" in implicit_concepts:
+        implicit_concepts.remove("")
     with open(concepts_path, "w") as concepts_csv:
         writer = csv.DictWriter(concepts_csv, concept_fields.keys())
         writer.writeheader()
         for row in concept_csv_content:
             writer.writerow(row)
-        for concept in orphaned_concepts:
+        for concept in implicit_concepts:
             concept_fields["name"] = concept
             writer.writerow(concept_fields)
--- tests/test_concepts.py
+++ tests/test_concepts.py
@@ -10,51 +10,87 @@ from paneldata_pipeline.concepts import extract_implicit_concepts
 
 
 class TestExtractImplicitConcepts(unittest.TestCase):
-    """ Test the extraction of concepts only present in the variables.csv"""
+    """ Test the extraction of concepts only present in variables and question data"""
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.sandbox_directory = Path(mkdtemp()).absolute()
         data_path = Path("./tests/test_data/").absolute()
         copy(data_path.joinpath("concepts.csv"), self.sandbox_directory)
         copy(data_path.joinpath("variables.csv"), self.sandbox_directory)
+        copy(data_path.joinpath("questions.csv"), self.sandbox_directory)
         self.concepts_path = self.sandbox_directory.joinpath("concepts.csv")
         self.variables_path = self.sandbox_directory.joinpath("variables.csv")
+        self.questions_path = self.sandbox_directory.joinpath("questions.csv")
 
         return super().setUp()
 
-    def test_variable_import_with_orphaned_concept(self):
-        """Are the concepts from the variables.csv added to the concepts.csv?"""
+    def test_extract_implicit_concept(self) -> None:
+        """Are the implicit concepts added to the concepts.csv?"""
         with open(self.concepts_path, "r") as csv_file:
             concept_names = {row["name"] for row in csv.DictReader(csv_file)}
         with open(self.variables_path, "r") as csv_file:
-            variable_concept_names = {row["concept"] for row in csv.DictReader(csv_file)}
-        self.assertLess(len(concept_names), len(variable_concept_names))
+            implicit_concept_names = {row["concept"] for row in csv.DictReader(csv_file)}
+        with open(self.questions_path, "r") as csv_file:
+            implicit_concept_names.update(
+                {row["concept"] for row in csv.DictReader(csv_file)}
+            )
 
-        extract_implicit_concepts(self.concepts_path, self.variables_path)
+        self.assertLess(len(concept_names), len(implicit_concept_names))
+
+        extract_implicit_concepts(
+            input_path=self.sandbox_directory, concepts_path=self.concepts_path
+        )
 
         with open(self.concepts_path, "r") as csv_file:
             concept_names = {row["name"] for row in csv.DictReader(csv_file)}
 
-        self.assertSetEqual(variable_concept_names, concept_names)
+        self.assertSetEqual(implicit_concept_names, concept_names)
 
-    def test_variable_import_without_concept_csv(self):
+    def test_extract_implicit_concept_without_concepts_path(self) -> None:
+        """Does the `concepts_path` argument have the correct default value?"""
+        with open(self.concepts_path, "r") as csv_file:
+            concept_names = {row["name"] for row in csv.DictReader(csv_file)}
+        with open(self.variables_path, "r") as csv_file:
+            implicit_concept_names = {row["concept"] for row in csv.DictReader(csv_file)}
+        with open(self.questions_path, "r") as csv_file:
+            implicit_concept_names.update(
+                {row["concept"] for row in csv.DictReader(csv_file)}
+            )
+
+        self.assertLess(len(concept_names), len(implicit_concept_names))
+
+        extract_implicit_concepts(input_path=self.sandbox_directory)
+
+        with open(self.concepts_path, "r") as csv_file:
+            concept_names = {row["name"] for row in csv.DictReader(csv_file)}
+
+        self.assertSetEqual(implicit_concept_names, concept_names)
+
+    def test_variable_import_without_concept_csv(self) -> None:
         """Is a concepts.csv added if it did not exist?"""
 
         remove(self.concepts_path)
 
         with open(self.variables_path, "r") as csv_file:
-            variable_concept_names = {row["concept"] for row in csv.DictReader(csv_file)}
+            implicit_concept_names = {row["concept"] for row in csv.DictReader(csv_file)}
+        with open(self.questions_path, "r") as csv_file:
+            implicit_concept_names.update(
+                {row["concept"] for row in csv.DictReader(csv_file)}
+            )
 
-        extract_implicit_concepts(self.concepts_path, self.variables_path)
+        extract_implicit_concepts(
+            input_path=self.sandbox_directory, concepts_path=self.concepts_path
+        )
 
         with open(self.concepts_path, "r") as csv_file:
             dict_reader = csv.DictReader(csv_file)
-            concept_file_header = dict_reader.fieldnames
+            if dict_reader.fieldnames:
+                concept_file_header = list(dict_reader.fieldnames)
             concept_names = {row["name"] for row in dict_reader}
 
-        self.assertSetEqual(variable_concept_names, concept_names)
+        self.assertSetEqual(implicit_concept_names, concept_names)
         self.assertListEqual(["name", "topic", "label_de", "label"], concept_file_header)
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         rmtree(self.sandbox_directory)
         return super().tearDown()
